7520a06b919da2271f9001d27b61ab4e81beb05b
brayokenya <kiirubrian21@gmail.com>
2020-05-03 14:45:14 +0300

    Added functional complete function for each goal

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Goal } from '../goal';
 
 @Component({
@@ -8,6 +8,11 @@ import { Goal } from '../goal';
 })
 export class GoalDetailComponent implements OnInit {
   @Input() goal: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
   constructor() {}
 
   ngOnInit() {}
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -22,6 +22,11 @@ export class GoalComponent implements OnInit {
   toggleDetails(index) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
+  completeGoal(isComplete, index) {
+    if (isComplete) {
+      this.goals.splice(index, 1);
+    }
+  }
   constructor() {}
 
   ngOnInit() {}
