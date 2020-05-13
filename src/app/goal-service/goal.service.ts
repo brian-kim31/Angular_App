73b04720aaa4b476118dfd25e2518e11a99249ac
brayokenya <kiirubrian21@gmail.com>
2020-05-13 13:00:18 +0300

    fixed array display error

--- src/app/goal-service/goal.service.ts
+++ src/app/goal-service/goal.service.ts
@@ -5,27 +5,30 @@ import { Goal } from '../goal';
   providedIn: 'root',
 })
 export class GoalService {
+  goalsArray = [
+    {
+      id: 1,
+      name: 'brian',
+      description: 'i jumped',
+      completeDate: new Date(2020, 3, 30),
+      showDescription: false,
+    },
+    {
+      id: 2,
+      name: 'Watch finding Nemo',
+      description: 'Find an online version and watch merlin find his son',
+      completeDate: new Date(2020, 3, 14),
+      showDescription: false,
+    },
+  ];
+
   getGoals() {
-    return [
-      {
-        id: 1,
-        name: 'brian',
-        description: 'i jumped',
-        completeDate: new Date(2020, 3, 30),
-        showDescription: false,
-      },
-      {
-        id: 2,
-        name: 'Watch finding Nemo',
-        description: 'Find an online version and watch merlin find his son',
-        completeDate: new Date(2020, 3, 14),
-        showDescription: false,
-      },
-    ];
+    return this.goalsArray;
   }
 
   getGoal(id) {
-    for (let goal of [Goal, id]) {
+    // for (let goal of [Goal, id])
+    for (let goal of this.goalsArray) {
       if (goal.id == id) {
         return goal;
       }
